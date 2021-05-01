64f891f026532d90fe6bd81c19e1f822f01dfb58
Raajeev Chandran <raajeevchandran787@gmail.com>
2021-05-01 19:26:49 +0530

    firebase init

---
+++ src/components/authentication/authentication.js
@@ -0,0 +1,94 @@
+import React from "react";
+import "./authentication.css";
+import { Login, Register } from "./index";
+import styled from "styled-components"
+
+
+const AuthenticationContainer = styled.div`
+display:flex;
+flexDirection:row;
+width:100%;
+height:100vh;`;
+
+const LeftSideContainer = styled.div`width:40%;`;
+
+
+class Authentication extends React.Component {  
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLogginActive: true
+    };
+  }
+
+  componentDidMount() {
+    this.rightSide.classList.add("right");
+  }
+
+  changeState() {
+    const { isLogginActive } = this.state;
+
+    if (isLogginActive) {
+      this.rightSide.classList.remove("right");
+      this.rightSide.classList.add("left");
+    } else {
+      this.rightSide.classList.remove("left");
+      this.rightSide.classList.add("right");
+    }
+    this.setState(prevState => ({ isLogginActive: !prevState.isLogginActive }));
+  }
+
+  render() {
+    const { isLogginActive } = this.state;
+    const current = isLogginActive ? "Register" : "Login";
+    const currentActive = isLogginActive ? "login" : "register";
+    return (
+      <AuthenticationContainer>
+        <LeftSideContainer>
+        
+            <img style={{width:"100%",height:"100%",objectFit:"contain"}} src="https://cdn.dribbble.com/users/4427600/screenshots/11185627/media/e26b8f4f94fe22b99ced6f26eac9b95d.png" alt=""/>
+
+        </LeftSideContainer>
+        
+        <div style={{width:"60%",borderRadius:"30px 0 0 30px",backgroundColor: "#12130f",display:"flex",justifyContent:"center",alignContent:"center",alignItems:"center"}}>
+
+        
+        <div className="App">
+        <div className="login">
+          <div className="container" ref={ref => (this.container = ref)}>
+            {isLogginActive && (
+              <Login containerRef={ref => (this.current = ref)} />
+            )}
+            {!isLogginActive && (
+              <Register containerRef={ref => (this.current = ref)} />
+            )}
+          </div>
+          <RightSide
+            current={current}
+            currentActive={currentActive}
+            containerRef={ref => (this.rightSide = ref)}
+            onClick={this.changeState.bind(this)}
+          />
+        </div>
+      </div>
+      </div>
+      </AuthenticationContainer>
+    );
+  }
+}
+
+const RightSide = props => {
+  return (
+    <div
+      className="right-side"
+      ref={props.containerRef}
+      onClick={props.onClick}
+    >
+      <div className="inner-container">
+        <div className="text">{props.current}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Authentication;
